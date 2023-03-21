e3c3f95008b4cfcfae9717389238993656d68f77
RorschachXR <53089452+RorschachXR@users.noreply.github.com>
2023-03-21 21:37:47 +0800

    Add files via upload

---
+++ Assignment4/Project 1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Project_1
+{
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+        public GenericList()
+        {
+            tail = head = null;
+        }
+        public Node<T> Head
+        {
+            get => head;
+        }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null) head = tail = n;
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action<T> action)
+        {
+            Node<T> node = head;
+            while (node != null)
+            {
+                action(node.Data);
+                node = node.Next;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> list = new GenericList<int>();
+            list.Add(1);
+            list.Add(1);
+            list.Add(4);
+            list.Add(5);
+            list.Add(1);
+            Console.WriteLine("List:");
+            list.ForEach(i => Console.Write(i + " "));
+            Console.WriteLine();
+            int sum = 0;
+            int max = int.MinValue;
+            int min = int.MaxValue;
+            list.ForEach(i =>
+            {
+                sum += i;
+                if (i > max) max = i;
+                if (i < min) min = i;
+            });
+            Console.WriteLine("Sum: " + sum);
+            Console.WriteLine("Max: " + max);
+            Console.WriteLine("Min: " + min);
+        }
+    }
+}
---
+++ Assignment4/Project2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace AlarmClock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AlarmClock clock = new AlarmClock();
+            clock.Tick += Clock_Tick;
+            clock.Alarm += Clock_Alarm;
+            clock.Start();
+        }
+        private static void Clock_Tick(object sender, EventArgs e)
+        {
+            Console.WriteLine("Tick...");
+        }
+        private static void Clock_Alarm(object sender, EventArgs e)
+        {
+            Console.WriteLine("Alarm!");
+        }
+    }
+    class AlarmClock
+    {
+        public event EventHandler Tick;
+        public event EventHandler Alarm;
+        public void Start()
+        {
+            while (true)
+            {
+                System.Threading.Thread.Sleep(1000);
+                OnTick();
+                if (DateTime.Now.Hour == 7 && DateTime.Now.Minute == 30)
+                {
+                    OnAlarm();
+                }
+            }
+        }
+        protected virtual void OnTick()
+        {
+            Tick?.Invoke(this, EventArgs.Empty);
+        }
+        protected virtual void OnAlarm()
+        {
+            Alarm?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
